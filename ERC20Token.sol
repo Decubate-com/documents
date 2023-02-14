82a7682da155f7b6cc67fe315da978f9f6599e11
Jay Maree <jaymaree@Jays-MacBook-M1-Pro.local>
2023-02-14 23:29:17 +0100

    updated ERC20 whitelisted contracts and added BRIKN audit report

---
+++ ERC20Token.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+
+//** Whitelisted ERC20 TOKEN for Mainnet */
+
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "./Whitelisted.sol";
+
+contract ERC20Token is ERC20, Whitelisted {
+
+  address public constant DEAD_ADDRESS = address(0xdead);
+
+  /**
+   *
+   * @dev mint initialSupply in constructor with symbol and name
+   *
+   */
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint256 _initialSupply,
+    uint256 _time,
+    uint256 _startTime,
+    uint256 _blockSellTime,
+    address _router,
+    address _liquidityToken
+  )
+    ERC20(_name, _symbol)
+    Whitelisted(_time, _startTime, _blockSellTime)
+  {
+    _mint(_msgSender(), _initialSupply);
+
+    IUniswapV2Router02 router = IUniswapV2Router02(_router);
+
+    address pair = IUniswapV2Factory(router.factory()).createPair(
+      address(this),
+      _liquidityToken
+    );
+
+    setPairAddress(pair, true);
+  }
+
+  /**
+   *
+   * @dev lock tokens by sending to DEAD address
+   *
+   */
+  function lockTokens(uint256 amount) external onlyOwner returns (bool) {
+    _transfer(_msgSender(), DEAD_ADDRESS, amount);
+    return true;
+  }
+
+  /**
+   * @dev Destroys `amount` tokens from the caller.
+   *
+   * See {ERC20-_burn}.
+   */
+  function burn(uint256 amount) external returns (bool) {
+    _burn(_msgSender(), amount);
+    return true;
+  }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be to transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     */
+
+    /* solhint-disable */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    )
+        internal
+        override
+        notBlackListed(from, to)
+        isTimeLocked(from, to)
+        isSaleBlocked(from, to)
+    {}
+    /* solhint-enable */
+}
---
+++ Whitelisted.sol
@@ -0,0 +1,205 @@
+// SPDX-License-Identifier: MIT
+
+//** Decubate Whitelisted Contract */
+//** Author Aceson */
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Whitelisted is Context, Ownable {
+    mapping(address => bool) public isWhitelisted;
+    mapping(address => bool) public isBlackListed;
+    mapping(address => bool) public isPair;
+
+    uint256 public startTime;
+    uint256 public blockSellUntil;
+
+    bool public isBlackListEnabled;
+    bool public isTimeLockEnabled;
+
+    event WhiteListSet(address addr, bool value);
+    event BlackListSet(address addr, bool value);
+    event BulkBlackList(address[] addr, bool[] value);
+    event BlackListEnabled(bool value);
+    event TimeLockEnabled(bool value);
+
+    //Modifier which allows only whitelisted addresses
+    modifier onlyWhiteListed() {
+        require(isWhitelisted[_msgSender()], "Caller is not whitelister");
+        _;
+    }
+
+    //Modifier which allows only non blacklisted addresses
+    modifier notBlackListed(address from, address to) {
+        if (isBlackListEnabled) {
+            require(
+                !isBlackListed[from] && !isBlackListed[to],
+                "Address is blacklisted"
+            );
+        }
+        _;
+    }
+
+    //Modifier which controls transfer on a set time period
+    modifier isTimeLocked(address from, address to) {
+        if (isTimeLockEnabled) {
+            if (!isWhitelisted[from] && !isWhitelisted[to]) {
+                require(
+                    block.timestamp >= startTime,
+                    "Trading not enabled yet"
+                );
+            }
+        }
+        _;
+    }
+
+    //Modifier which blocks sell until blockSellUntil value
+    modifier isSaleBlocked(address from, address to) {
+        if (!isWhitelisted[from] && isPair[to]) {
+            require(block.timestamp >= blockSellUntil, "Sell disabled!");
+        }
+        _;
+    }
+
+    //time - amount of time in seconds before trading is enabled from
+    constructor(
+        uint256 time,
+        uint256 _startTime,
+        uint256 _blockSellUntil
+    ) {
+        isWhitelisted[_msgSender()] = true;
+        isBlackListEnabled = true;
+        isTimeLockEnabled = true;
+
+        startTime = _startTime + time;
+        blockSellUntil = _blockSellUntil;
+    }
+
+    /**
+     *
+     * @dev Include/Exclude an address in whitelist
+     *
+     * @param {addr} Address of user
+     * @param {value} Whitelist status
+     *
+     * @return {bool} Status of whitelisting
+     *
+     */
+    function whiteList(address addr, bool value)
+        external
+        onlyOwner
+        returns (bool)
+    {
+        isWhitelisted[addr] = value;
+        emit WhiteListSet(addr, value);
+        return true;
+    }
+
+    /**
+     *
+     * @dev Include/Exclude an address in bllacklist
+     *
+     * @param {addr} Address of user
+     * @param {value} Blacklist status
+     *
+     * @return {bool} Status of blacklisting
+     *
+     */
+    function blackList(address addr, bool value)
+        external
+        onlyWhiteListed
+        returns (bool)
+    {
+        isBlackListed[addr] = value;
+        emit BlackListSet(addr, value);
+        return true;
+    }
+
+    /**
+     *
+     * @dev Include/Exclude multiple address in blacklist
+     *
+     * @param {addr} Address array of users
+     * @param {value} Whitelist status of users
+     *
+     * @return {bool} Status of bulk blacklist
+     *
+     */
+    function bulkBlackList(address[] calldata addr, bool[] calldata value)
+        external
+        onlyWhiteListed
+        returns (bool)
+    {
+        require(addr.length == value.length, "Array length mismatch");
+        uint256 len = addr.length;
+
+        for (uint256 i = 0; i < len; i++) {
+            isBlackListed[addr[i]] = value[i];
+        }
+
+        emit BulkBlackList(addr, value);
+        return true;
+    }
+
+    /**
+     *
+     * @dev Enable/disable blacklist usage in contract
+     *
+     * @param {value} Set/remove blacklist
+     *
+     * @return {bool} Status of enable/disable
+     *
+     */
+    function setBlackList(bool value) external onlyWhiteListed returns (bool) {
+        isBlackListEnabled = value;
+        emit BlackListEnabled(value);
+        return true;
+    }
+
+    /**
+     *
+     * @dev Enable/disable timelock usage in contract
+     *
+     * @param {value} Set/remove timelock
+     *
+     * @return {bool} Status of enable/disable
+     *
+     */
+    function setTimeLocked(bool _isTimeLockEnabled, uint256 _startTime)
+        external
+        onlyWhiteListed
+        returns (bool)
+    {
+        isTimeLockEnabled = _isTimeLockEnabled;
+        startTime = _startTime;
+        emit TimeLockEnabled(_isTimeLockEnabled);
+        return true;
+    }
+
+    /**
+     *
+     * @dev Set blockSellUntil
+     *
+     * @param {value} time to block sales until
+     *
+     * @return {bool} Status of enable/disable
+     *
+     */
+    function setBlockSellUntil(uint256 value)
+        external
+        onlyWhiteListed
+        returns (bool)
+    {
+        blockSellUntil = value;
+        return true;
+    }
+
+    /// @notice Sets status of an address to pair or not
+    /// @dev true = is pair, false = not pair
+    /// @param addr Address you want to change status
+    /// @param status Whether pair or not
+    function setPairAddress(address addr, bool status) internal {
+        isPair[addr] = status;
+    }
+}
